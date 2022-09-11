937c5b1a572a9d8dadfa9c13eeb1d16123d45dc7
Karl Mainville <2038207@etu.cegepsth.qc.ca>
2022-09-10 23:13:04 -0400

    Ajout du footer et le lien avec les pages "About" et "Contact"

--- LemadCorp/LemadWeb/Controllers/HomeController.cs
+++ LemadCorp/LemadWeb/Controllers/HomeController.cs
@@ -23,6 +23,16 @@ namespace LemadWeb.Controllers
             return View();
         }
 
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
         public IActionResult Privacy()
         {
             return View();
