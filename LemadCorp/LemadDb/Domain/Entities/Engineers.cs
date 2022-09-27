28f1e04e8ca2a363a0eb5edb250d09af74e66009
Karl Mainville <2038207@etu.cegepsth.qc.ca>
2022-09-27 08:20:33 -0400

    Ajout de la validation des modèles

--- LemadCorp/LemadDb/Domain/Entities/Engineers.cs
+++ LemadCorp/LemadDb/Domain/Entities/Engineers.cs
@@ -4,6 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FluentValidation;
 using static LemadDb.Data.Status;
 
 namespace LemadDb.Domain.Entities
@@ -13,24 +14,42 @@ namespace LemadDb.Domain.Entities
         [Required]
         public int Id { get; set; }
 
-        [Required]
-        public string Name { get; set; }
+        public string FirstName { get; set; }
 
-        [Required]
         public string LastName { get; set; }
 
         #region Stats
 
         #endregion
 
-        [Required]
         [DataType(dataType: DataType.Currency)]
         public decimal Price { get; set; }
 
-        [Required]
         public byte Discount { get; set; }
 
         [Required]
         public ProductStatus Status { get; set; }
+    }
+
+    public class EngineerValidator : AbstractValidator<Engineers>
+    {
+        public EngineerValidator()
+        {
+            RuleFor(e => e.FirstName)
+                .NotEmpty()
+                .WithMessage("The firstname cannot be empty!");
+
+            RuleFor(e => e.LastName)
+                .NotEmpty()
+                .WithMessage("The lastname cannot be empty!");
+
+            RuleFor(e => e.Price)
+                .NotEmpty().WithMessage("The price cannot be empty!")
+                .GreaterThan(0).WithMessage("The price must be higher than 0.0$");
+
+            RuleFor(e => e.Discount)
+                .NotEmpty().WithMessage("The discount cannot be empty!")
+                .When(e => e.Discount >= 0 && e.Discount <= 100).WithMessage("The discount must be greater or equal and 0 and less or equal than 100");
+        }
     }
 }
--- LemadCorp/LemadDb/Domain/Entities/PowerUnits.cs
+++ LemadCorp/LemadDb/Domain/Entities/PowerUnits.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using FluentValidation;
 using static LemadDb.Data.Status;
 
 namespace LemadDb.Domain.Entities
@@ -8,21 +9,36 @@ namespace LemadDb.Domain.Entities
         [Required]
         public int Id { get; set; }
 
-        [Required]
         public string Name { get; set; }
 
         #region Stats
 
         #endregion
 
-        [Required]
         [DataType(dataType: DataType.Currency)]
         public decimal Price { get; set; }
 
-        [Required]
         public byte Discount { get; set; }
 
         [Required]
         public ProductStatus Status { get; set; }
+    }
+
+    public class PowerUnitValidator : AbstractValidator<PowerUnits>
+    {
+        public PowerUnitValidator()
+        {
+            RuleFor(e => e.Name)
+                .NotEmpty()
+                .WithMessage("The name cannot be empty!");
+
+            RuleFor(e => e.Price)
+                .NotEmpty().WithMessage("The price cannot be empty!")
+                .GreaterThan(0).WithMessage("The price must be higher than 0.0$");
+
+            RuleFor(e => e.Discount)
+                .NotEmpty().WithMessage("The discount cannot be empty!")
+                .When(e => e.Discount >= 0 && e.Discount <= 100).WithMessage("The discount must be greater or equal and 0 and less or equal than 100");
+        }
     }
 }
