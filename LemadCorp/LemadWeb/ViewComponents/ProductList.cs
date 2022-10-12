fe1762b66d1d34af501217cf1f4f5068b09756e1
Karl Mainville <2038207@etu.cegepsth.qc.ca>
2022-10-12 15:39:56 -0400

    Ajout de la logique du search

--- LemadCorp/LemadWeb/ViewComponents/ProductList.cs
+++ LemadCorp/LemadWeb/ViewComponents/ProductList.cs
@@ -19,34 +19,38 @@ namespace LemadWeb.ViewComponents
         {
             if (product == "PILOT") {
                 var item = await _context.pilots.ToListAsync();
-                if (search != null)
-                    return View(item.Where(c => c.FirstName.Contains(search) || c.LastName.Contains(search)));
-                else 
-                    return View("pilot", item);
-            } else if (product == "ENGINEER") {
+                if (!string.IsNullOrEmpty(search))
+                    item = item.Where(c => c.FirstName.Contains(search) || c.LastName.Contains(search)).ToList();
+                ViewBag.Verification = (item.Count() > 0) ? true : false;
+                return View("pilot", item);
+            } 
+            else if (product == "ENGINEER") {
                 var item = await _context.engineers.ToListAsync();
-                if (search != null)
-                    return View(item.Where(c => c.FirstName.Contains(search) || c.LastName.Contains(search)));
-                else
-                    return View("engineer", item);
-            } else if (product == "MECHANIC") {
+                if (!string.IsNullOrEmpty(search))
+                    item = item.Where(c => c.FirstName.Contains(search) || c.LastName.Contains(search)).ToList();
+                ViewBag.Verification = (item.Count() > 0) ? true : false;
+                return View("engineer", item);
+            } 
+            else if (product == "MECHANIC") {
                 var item = await _context.mechanics.ToListAsync();
-                if (search != null)
-                    return View(item.Where(c => c.FirstName.Contains(search) || c.LastName.Contains(search)));
-                else
-                    return View("mechanic", item);
-            } else if (product == "PRINCIPAL") {
+                if (!string.IsNullOrEmpty(search))
+                    item = item.Where(c => c.FirstName.Contains(search) || c.LastName.Contains(search)).ToList();
+                ViewBag.Verification = (item.Count() > 0) ? true : false;
+                return View("mechanic", item);
+            } 
+            else if (product == "PRINCIPAL") {
                 var item = await _context.principals.ToListAsync();
-                if (search != null)
-                    return View(item.Where(c => c.FirstName.Contains(search) || c.LastName.Contains(search)));
-                else
-                    return View("principal", item);
-            } else if (product == "POWERUNIT") {
+                if (!string.IsNullOrEmpty(search))
+                    item = item.Where(c => c.FirstName.Contains(search) || c.LastName.Contains(search)).ToList();
+                ViewBag.Verification = (item.Count() > 0) ? true : false;
+                return View("principal", item);
+            } 
+            else if (product == "POWERUNIT") {
                 var item = await _context.powerUnits.ToListAsync();
-                if (search != null)
-                    return View(item.Where(c => c.Name.Contains(search)));
-                else
-                    return View("powerunit", item);
+                if (!string.IsNullOrEmpty(search))
+                    item = item.Where(c => c.Name.Contains(search)).ToList();
+                ViewBag.Verification = (item.Count() > 0) ? true : false;
+                return View("powerunit", item);
             }
             else
                 return View();
