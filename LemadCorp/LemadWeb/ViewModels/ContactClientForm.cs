dda59c2f0a1931a9677a7b7ede9b4f3b2cb7974c
Karl Mainville <2038207@etu.cegepsth.qc.ca>
2022-10-26 22:04:56 -0400

    Changement de la page d'accueil

--- LemadCorp/LemadWeb/ViewModels/ContactClientForm.cs
+++ LemadCorp/LemadWeb/ViewModels/ContactClientForm.cs
@@ -1,14 +1,28 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using FluentValidation;
 
 namespace LemadWeb.ViewModels
 {
     public class ContactClientForm
     {
-        [Required]
+        [Display(Name = "Name")]
         public string FullName { get; set; }
-        [Required]
+
         [RegularExpression(@"^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$", ErrorMessage ="This is not a valid email address")]
         public string Email { get; set; }
+
         public string Message { get; set; }
+    }
+
+    public class ContactClientFormValidator : AbstractValidator<ContactClientForm>
+    {
+        public ContactClientFormValidator()
+        {
+            RuleFor(c => c.FullName)
+                .NotNull().WithMessage("The name is required to contact us");
+
+            RuleFor(c => c.Email)
+                .NotNull().WithMessage("The email is required to contact us");
+        }
     }
 }
