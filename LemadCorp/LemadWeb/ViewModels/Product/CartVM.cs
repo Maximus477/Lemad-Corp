80f64f1ff33b7b2806e99f31bfc7f4aed2941c2e
Karl Mainville <2038207@etu.cegepsth.qc.ca>
2022-10-30 13:57:41 -0400

    finition du fonctionnement de cart et autre.

--- LemadCorp/LemadWeb/ViewModels/Product/CartVM.cs
+++ LemadCorp/LemadWeb/ViewModels/Product/CartVM.cs
@@ -1,7 +1,12 @@
-﻿namespace LemadWeb.ViewModels.Product
+﻿using System.Collections.Generic;
+
+namespace LemadWeb.ViewModels.Product
 {
     public class CartVM
     {
-        
+        public Dictionary<string, string> Products { get; set; }
+
+        public CartVM(Dictionary<string, string> products)
+        { Products = products; }
     }
 }
