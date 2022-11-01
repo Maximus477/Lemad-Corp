4f2564ce56ca1923c3210afc0ccd017a3292612a
Louis Garceau <2076183@etu.cegepsth.qc.ca>
2022-10-31 20:19:37 -0400

    Fix du authenticate et debut de creation de commande

---
+++ LemadCorp/LemadWeb/ViewModels/Product/CommandVM.cs
@@ -0,0 +1,13 @@
+﻿using LemadDb.Domain.User;
+
+namespace LemadWeb.ViewModels.Product
+{
+    public class CommandVM
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public AdresseCivique Address { get; set; }
+    }
+}
